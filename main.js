81f45826c9d21c94102b6cc012cc887f513b272a
luanamoraes260407 <105320286+luanamoraes260407@users.noreply.github.com>
2024-03-18 10:25:31 -0300

    ok

--- main.js
+++ main.js
@@ -13,5 +13,5 @@ for(let i=0;i <botoes.length;i++){
 }
 
 const contadores = document.querySelectorAll("contador");
-const tempoObjetivo1 = new Date("2024-12-31")
+const tempoObjetivo1 = new Date("2024-12-31T00;00;00")
 contadores[0].textcontent = tempoObjetivo1
