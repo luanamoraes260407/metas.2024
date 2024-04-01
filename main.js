19d9b9724f652aea4ebd5ca65091bc945ff4ee2d
luanamoraes260407 <105320286+luanamoraes260407@users.noreply.github.com>
2024-04-01 10:14:02 -0300

    ok

--- main.js
+++ main.js
@@ -16,3 +16,9 @@ const contadores = document.querySelectorAll("contador");
 const tempoObjetivo1 = new Date("2024-12-31T00:00:00");
 let tempoAtual = new Date ()
 contadores[0].textcontent = tempoObjetivo1;
+
+let tempoFinal = tempoObjetivo1 - tempoAtual;
+let segundos = tempoFinal/1000;
+let minutos = segundos/60;
+let horas = minutos/60;
+let dias = horas/24;
