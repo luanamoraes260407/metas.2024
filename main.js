96c6b37002de2f27492d2970ffa903f5484b44cf
luanamoraes260407 <105320286+luanamoraes260407@users.noreply.github.com>
2024-04-05 10:59:32 -0300

    ok

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 const botoes = document.querySelectorAll(".botao");
-const textos=document.querySelectorAll(".aba-conteudo")
-for(let i=0;i <botoes.length;i++){
-    botoes[i].onclick = function(){
-        for(let j=0;j<botoes.length;j++){
+const textos = document.querySelectorAll(".aba-conteudo")
+for (let i = 0; i < botoes.length; i++) {
+    botoes[i].onclick = function () {
+        for (let j = 0; j < botoes.length; j++) {
             botoes[j].classList.remove("ativo");
             botoes[j].classList.remove("ativo");
         }
@@ -14,22 +14,23 @@ for(let i=0;i <botoes.length;i++){
 
 const contadores = document.querySelectorAll("contador");
 const tempoObjetivo1 = new Date("2024-12-31T00:00:00");
-let tempoAtual = new Date ()
-//contadores[0].textcontent = tempoObjetivo1;
 
-//function calculaTempo(tempoObjetivo) {
+contadores[0].textcontent = tempoObjetivo1;
 
-//}
+function calculaTempo(tempoObjetivo) {
+    let tempoAtual = new Date()
+    let tempoFinal = tempoObjetivo1 - tempoAtual;
+    let segundos = Math.floor(tempoFinal / 1000);
+    let minutos = Math.floor(segundos / 60);
+    let horas = Math.floor(minutos / 60);
+    let dias = Math.floor(horas / 24);
 
-let tempoFinal = tempoObjetivo1 - tempoAtual;
-let segundos = Math.floor(tempoFinal/1000);
-let minutos = Math.floor(segundos/60);
-let horas = Math.floor(minutos/60);
-let dias = Math.floor(horas/24);
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 24;
 
-segundos %=60;
-minutos %=60;
-horas %=24;
+    contadores[0].textContent = dias + " dias " + horas + " horas " +
+        minutos + " minutos " + segundos + " segundos ";
+    //}
 
-contadores[0]. textContent = dias + " dias " + horas + " horas " +
-minutos + " minutos " + segundos " segundos ";
+}
